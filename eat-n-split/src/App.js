22447dc88e866c5b2d1e9fc2298d7b7795515ed9
Caylin James <cjames@leapagency.com>
2024-01-24 14:04:32 -0500

    set up the final controlled from elements for the project

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -148,21 +148,45 @@ function FormAddFriend({ onAddFriend }) {
 }
 
 function FormSplitBill({ friend }) {
+    const [bill, setBill] = useState('');
+    const [userExpense, setUserExpense] = useState('');
+    const friendExpense = bill ? bill - userExpense : '';
+    const [payer, setPayer] = useState('user');
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(e);
+    }
+
     return (
-        <form className='form-split-bill'>
+        <form className='form-split-bill' onSubmit={handleSubmit}>
             <h2>Split a bill with {friend.name}</h2>
 
             <label>💰 Bill value</label>
-            <input type='text' />
+            <input
+                type='text'
+                value={bill}
+                onChange={(e) => setBill(Number(e.target.value))}
+            />
 
             <label>💵 Your expense</label>
-            <input type='text' />
+            <input
+                type='text'
+                value={userExpense}
+                onChange={(e) =>
+                    setUserExpense(
+                        Number(e.target.value) > bill
+                            ? userExpense
+                            : Number(e.target.value),
+                    )
+                }
+            />
 
             <label>👴 {friend.name}'s expense</label>
-            <input type='text' disabled />
+            <input type='text' disabled value={friendExpense} />
 
             <label>🤑 Who is paying the bill?</label>
-            <select>
+            <select value={payer} onChange={(e) => setPayer(e.target.value)}>
                 <option value='user'>You</option>
                 <option value='friend'>{friend.name}</option>
             </select>
