b1a5c18e1a4ae96c7236308881c77126ce6dbfe1
Caylin James <caylinjames.dev@gmail.com>
2024-01-23 20:12:45 -0500

    more practice with using the children prop

--- steps/src/App.js
+++ steps/src/App.js
@@ -48,9 +48,7 @@ function Steps() {
                         <div className={step >= 3 ? 'active' : ''}>3</div>
                     </div>
 
-                    <p className='message'>
-                        Step {step}: {messages[step - 1]}
-                    </p>
+                    <StepMessage step={step}>{messages[step - 1]}</StepMessage>
 
                     <div className='buttons'>
                         <Button
@@ -70,6 +68,15 @@ function Steps() {
                     </div>
                 </div>
             )}
+        </div>
+    );
+}
+
+function StepMessage({ step, children }) {
+    return (
+        <div className='message'>
+            <h3>Step {step}:</h3>
+            {children}
         </div>
     );
 }
