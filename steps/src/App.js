1359af01fcf985864f497c378bb7931392437827
Caylin James <caylinjames.dev@gmail.com>
2024-01-23 20:05:07 -0500

    add the steps project

---
+++ steps/src/App.js
@@ -0,0 +1,89 @@
+import { useState } from 'react';
+
+const messages = [
+    'Learn React ⚛️',
+    'Apply for jobs 💼',
+    'Invest your new income 🤑',
+];
+
+export default function App() {
+    return (
+        <div>
+            <Steps />
+        </div>
+    );
+}
+
+function Steps() {
+    const [step, setStep] = useState(1);
+    const [isOpen, setIsOpen] = useState(true);
+
+    function handlePrevious() {
+        if (step > 1) {
+            setStep((currentStep) => currentStep - 1);
+        }
+    }
+
+    function handleNext() {
+        if (step < 3) {
+            setStep((currentStep) => currentStep + 1);
+        }
+    }
+
+    return (
+        <div>
+            <button
+                className='close'
+                onClick={() => {
+                    setIsOpen((open) => !open);
+                }}
+            >
+                &times;
+            </button>
+            {isOpen && (
+                <div className='steps'>
+                    <div className='numbers'>
+                        <div className={step >= 1 ? 'active' : ''}>1</div>
+                        <div className={step >= 2 ? 'active' : ''}>2</div>
+                        <div className={step >= 3 ? 'active' : ''}>3</div>
+                    </div>
+
+                    <p className='message'>
+                        Step {step}: {messages[step - 1]}
+                    </p>
+
+                    <div className='buttons'>
+                        <Button
+                            textColor='#fff'
+                            backgroundColor='#7950f2'
+                            onClick={handlePrevious}
+                        >
+                            <span>👈</span> Previous
+                        </Button>
+                        <Button
+                            textColor='#fff'
+                            backgroundColor='#7950f2'
+                            onClick={handleNext}
+                        >
+                            Next <span>👉</span>
+                        </Button>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
+
+function Button({ backgroundColor, textColor, onClick, children }) {
+    return (
+        <button
+            style={{
+                backgroundColor: backgroundColor,
+                color: textColor,
+            }}
+            onClick={onClick}
+        >
+            {children}
+        </button>
+    );
+}
