08d6782f918588a67c8a47b76ca9de33c1b24c07
Caylin James <caylinjames.dev@gmail.com>
2024-01-26 22:05:18 -0500

    look at some examples of state batching

--- how-react-works/src/App.js
+++ how-react-works/src/App.js
@@ -65,8 +65,25 @@ function TabContent({ item }) {
     const [showDetails, setShowDetails] = useState(true);
     const [likes, setLikes] = useState(0);
 
+    console.log('RENDER');
+
     function handleInc() {
-        setLikes(likes + 1);
+        setLikes((curLikes) => curLikes + 1);
+    }
+
+    function handleTripleInc() {
+        setLikes((curLikes) => curLikes + 1);
+        setLikes((curLikes) => curLikes + 1);
+        setLikes((curLikes) => curLikes + 1);
+    }
+
+    function handleUndo() {
+        setShowDetails(true);
+        setLikes(0);
+    }
+
+    function handleUndoLater() {
+        setTimeout(handleUndo, 2000);
     }
 
     return (
@@ -82,13 +99,13 @@ function TabContent({ item }) {
                 <div className='hearts-counter'>
                     <span>{likes} ❤️</span>
                     <button onClick={handleInc}>+</button>
-                    <button>+++</button>
+                    <button onClick={handleTripleInc}>+++</button>
                 </div>
             </div>
 
             <div className='tab-undo'>
-                <button>Undo</button>
-                <button>Undo in 2s</button>
+                <button onClick={handleUndo}>Undo</button>
+                <button onClick={handleUndoLater}>Undo in 2s</button>
             </div>
         </div>
     );
