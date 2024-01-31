c68c4a76c0656480c7f81ca5bc6b9e1efd7f5b51
Caylin James <caylinjames.dev@gmail.com>
2024-01-30 19:59:46 -0500

    introduce the useRef hook

--- usepopcorn/src/components/Search.js
+++ usepopcorn/src/components/Search.js
@@ -1,4 +1,27 @@
+import { useEffect, useRef } from 'react';
+
 export default function Search({ query, setQuery }) {
+    const inputEl = useRef(null);
+
+    useEffect(
+        function () {
+            function callback(e) {
+                if (document.activeElement === inputEl) return;
+
+                if (e.code === 'Enter') {
+                    inputEl.current.focus();
+                    setQuery('');
+                }
+            }
+
+            document.addEventListener('keydown', callback);
+            inputEl.current.focus();
+
+            return () => document.removeEventListener('keydown', callback);
+        },
+        [setQuery],
+    );
+
     return (
         <input
             className='search'
@@ -6,6 +29,7 @@ export default function Search({ query, setQuery }) {
             placeholder='Search movies...'
             value={query}
             onChange={(e) => setQuery(e.target.value)}
+            ref={inputEl}
         />
     );
 }
