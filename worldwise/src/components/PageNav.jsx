468e6ea400138c1a6fb62e5d190aafcf7c4937b5
Caylin James <caylinjames.dev@gmail.com>
2024-02-15 07:29:24 -0500

    make a couple of small changes

--- worldwise/src/components/PageNav.jsx
+++ worldwise/src/components/PageNav.jsx
@@ -1,11 +1,12 @@
-import { Link, NavLink } from 'react-router-dom';
-import styles from './PageNav.module.css';
+import { NavLink } from 'react-router-dom';
 import Logo from './Logo';
+import styles from './PageNav.module.css';
 
 export default function PageNav() {
   return (
     <nav className={styles.nav}>
       <Logo />
+
       <ul>
         <li>
           <NavLink to='/pricing'>Pricing</NavLink>
