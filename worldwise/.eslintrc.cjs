bcfa61675285369da30971150dbc66a031a4ccaf
Caylin James <caylinjames.dev@gmail.com>
2024-02-14 21:07:01 -0500

    implement main page and basic routes

--- worldwise/.eslintrc.cjs
+++ worldwise/.eslintrc.cjs
@@ -6,15 +6,14 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react/react-in-jsx-scope': 'off',
   },
-}
+};
--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,5 +1,18 @@
-import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import PageNotFound from './pages/PageNotFound';
 
 export default function App() {
-  return <div>WorldWise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='product' element={<Product />} />
+        <Route path='pricing' element={<Pricing />} />
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
